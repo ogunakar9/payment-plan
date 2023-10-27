f2393637cbd8adcfe2965816489d7b35ade90625
Ogun Akar <ogunakar09@gmail.com>
2023-10-27 13:46:57 +0300

    feat: add pro select input

--- src/components/Pro/index.tsx
+++ src/components/Pro/index.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+
 import { pro } from "../../data";
 import "../common/styles.scss";
+import "./styles.scss";
 
 const Pro = () => {
+  const [age, setAge] = useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value);
+  };
+
   return (
-    <div className="card">
+    <div className="card pro-section">
       <div className="card__header">
         <span className="card__header__title">Pro</span>
         <div>
@@ -11,15 +25,35 @@ const Pro = () => {
           <span className="card__header__division"> / month</span>
         </div>
       </div>
-      <ul className="card__list">
-        {pro.list.map((item: any, index: number) => {
-          return (
-            <li key={index} className="card__list__item">
-              {item}
-            </li>
-          );
-        })}
-      </ul>
+      <div className="pro-section__form">
+        <FormControl sx={{ m: 1, width: "100%" }}>
+          <InputLabel id="demo-simple-select-helper-label">Age</InputLabel>
+          <Select
+            labelId="demo-simple-select-helper-label"
+            id="demo-simple-select-helper"
+            value={age}
+            label="Age"
+            onChange={handleChange}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+          <FormHelperText>Monthly active users</FormHelperText>
+        </FormControl>
+        <ul className="card__list">
+          {pro.list.map((item: any, index: number) => {
+            return (
+              <li key={index} className="card__list__item">
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
       <button className="card__button pro">Choose plan</button>
     </div>
   );
